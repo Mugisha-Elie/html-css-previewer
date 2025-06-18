c5f7cb00f8204794d26360735e83f63b1f95ac40
Mugisha Elie <iradukunda2k24@gmail.com>
2025-06-18 19:18:26 +0200

    feat: auto-save user code to localStorage and load it on refresh

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
+//Select the textarea and preview elements
 const editor = document.getElementById('editor');
 const preview = document.getElementById('preview');
 
+//Load saved code from LocalStorage if available
+const savedCode = localStorage.getItem("savedCode");
+
+if(savedCode){
+    editor.value = savedCode;
+    preview.srcdoc = savedCode;
+}
+
 editor.addEventListener('input', () => {
-  const content = editor.value;
-  preview.srcdoc = content;
+  const code = editor.value;
+  localStorage.setItem("savedCode", code);
+  preview.srcdoc = code;
 });
